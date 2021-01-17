5b0be513791848c88dca269350141137c4bb44cb
Samir Omer <samiromer2@yahoo.com>
2021-01-17 03:41:12 -0800

    Add files via upload

---
+++ formexample.php
@@ -0,0 +1,72 @@
+<html>
+<body>
+	<table >
+<tr>
+	<td>name</td> 
+<td><input>	
+<input>	
+<input>	
+<input></td>	
+</tr>
+<tr><td>
+age </td>
+<td>
+<input></td></tr>	
+<tr><td>
+nat
+</td><td>
+<select>
+<option>San</option>
+<option>china</option>
+<option>Egpe</option>
+</select>
+</td></tr>
+<tr><td>
+date of birth</td><td>
+<select>
+<option>day</option>
+<option>1</option>
+<option>2</option>
+<option>3</option>
+<option>4</option>
+<option>5</option>
+</select>
+
+<select>
+<option>month</option>
+<option>1</option>
+<option>2</option>
+<option>3</option>
+<option>4</option>
+<option>5</option>
+</select>
+<select>
+<option>year</option>
+<option>2021</option>
+<option>2020</option>
+<option>2019</option>
+<option>2018</option>
+<option>2019</option>
+</select>
+</td></tr>
+<tr><td>
+Gender
+</td>
+<td>
+<input type = "radio">
+male
+<input type = "radio">
+Female
+</td></tr>
+<tr><td>
+<input type = "checkbox"></td><td>
+Do you agree?
+</td></tr>
+<tr><td>
+	</td>
+	<td>
+<input type = "submit" value = "Save">
+</td></tr>
+</table>
+</body>	
+</html>
