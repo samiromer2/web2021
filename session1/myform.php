051d742de80dd6a6b43341d39103ef614139e99a
Samir Omer <samiromer2@yahoo.com>
2021-01-19 14:37:03 +0200

    Create myform.php

---
+++ session1/myform.php
@@ -0,0 +1,58 @@
+<html>
+<title>
+    Super Market
+</title>
+
+<body>
+
+    <center> Super Market</center>
+    <h2>Stuff form</h2>
+    <form method="POST" action="myformdata.php">
+        <table>
+            <tr>
+                <td>staff</td>
+                <td><input name="staffname" type="text" required>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    phone </td>
+                <td>
+                    <input name="phone" type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    gender
+                </td>
+                <td>
+                    <select name="Gender">
+                        <option value="non">Gender</option>
+                        <option value="M">Male</option>
+                        <option value="F">Female</option>
+
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    address </td>
+                <td>
+                    <input name="address" type="text" required>
+                </td>
+            </tr>
+
+
+            <tr>
+                <td>
+                </td>
+                <td>
+                    <input type="submit" value="Save">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
