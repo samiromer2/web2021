a2399a0f0e1b44df014d33cb167270ddd1b49e57
Samir Omer <samiromer2@yahoo.com>
2021-01-19 14:38:02 +0200

    Create myformdata.php

---
+++ session1/myformdata.php
@@ -0,0 +1,39 @@
+<?php
+echo "hello";
+//any variable php $
+
+// echo $_POST;
+print_r($_POST);
+//function name -> print_r
+//array name $_POST
+echo "<br>";
+
+//variable 
+//$ variable name = 
+
+// = assign 
+// == equal
+// === null
+
+$gender = $_POST['Gender'] = 'M';
+
+//selection 
+//if (cond)
+//true 
+//else 
+//stat
+/*
+if ($gender  == 'M')
+    echo "Male";
+echo "hello";
+
+switch ($gender) {
+    case 'M':
+        echo "Male";
+        break;
+    case 'F':
+        echo "Female";
+        break;
+}
+*/
+echo "<br>";
